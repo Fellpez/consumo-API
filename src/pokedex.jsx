7eb9288b9c1ede89b4482632b21e015f5f2c8dae
Fellpez <contatofellipe17@gmail.com>
2026-09-10 00:41:01 -0300

    PokedexV2

--- src/pokedex.jsx
+++ src/pokedex.jsx
@@ -1,34 +1,77 @@
 import { useState, useEffect } from 'react';
+import './pokedex.css';
 
+function Pokedex() {
 
-function Pokedex(){
-
+    const [abrirPokedex, alteraAbrirPokedex] = useState(false);
     const [pokemon, alteraPokemon] = useState([]);
     const [pesquisa, alteraPesquisa] = useState('');
 
     async function pesquisarPokemon() {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pesquisa}`);
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + pesquisa);
         const data = await response.json();
         console.log(data);
         alteraPokemon(data);
     }
 
 
-    return(
+    return (
+
         <div>
-            <h1>Pokedex</h1>
-            <p>Consulte um Pokémon</p>
 
-            <input onChange={(e) => alteraPesquisa(e.target.value)} placeholder="Buscar Pokémon"/>
-            <button onClick={pesquisarPokemon}>🔎Pesquisar</button>
 
-            <hr/>
+            {abrirPokedex == true ? (
+              []
+            ) 
+            : 
+            (
+                <button className="botaoAbrir" onClick={() => alteraAbrirPokedex(true)}>
+                    Abrir Pokédex
+                </button>
+            )}
 
-            <h2>Nome: {pokemon.name}</h2>
-            <p>Tipo: </p>
+            {abrirPokedex && (
+                <>
+                <div className="pokedexGrupo">
+                    <button className="botaoFechar" onClick={() => alteraAbrirPokedex(false)}>
+                        Fechar Pokédex
+                    </button>
 
-            <img />
-            
+
+                <div className='cameraGrupo'>
+                    <div className='camera'></div>
+                    <div className='camera2'></div>
+                </div>
+
+                <div className="bordaVisor">
+                    <div className="visorPokedex">
+                        <h1 className="titulo">Pokédex</h1>
+                        <p className="descricao">Consulte um Pokémon</p>
+
+                        <input className="inputPesquisa" onChange={(e) => alteraPesquisa(e.target.value)} placeholder="Buscar Pokémon" />
+                        <button className="botaoPesquisar" onClick={pesquisarPokemon}>🔎Pesquisar</button>
+                    </div>
+
+                </div>
+
+                </div>
+
+            <div className="informacoesPokemon">
+                <div className="bordaVisorInformacoes">
+                    <div className="visorInformacoes">
+                        <h2 className="nomePokemon">Nome: {pokemon.name}</h2>
+                        <p className="tipoPokemon">Tipo: {pokemon.types?.[0]?.type.name}</p>
+
+                        <img className="imagem" src={pokemon.sprites?.front_default} width="250" />
+                      
+                        <p className="habilidadesPokemon">Habilidades: {pokemon.abilities?.[0]?.ability.name}</p>
+                    </div>
+                </div>
+            </div>
+                </>
+            )}
+
+
         </div>
     )
 }
